097500ca7880999d87128a3ac3c6efbbcdbcdd6d
Bunlong-Prak <106743681+PrakBunlong@users.noreply.github.com>
2024-05-12 13:02:34 +0700

    Minor changes

--- src/routes/hehe/+page.svelte
+++ src/routes/hehe/+page.svelte
@@ -1,93 +1,89 @@
 <body class="w-full pt-12 flex justify-center items-center dark:bg-gray-900">
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <p
+            class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white py-7"
+        >
+            Wallet Page
+        </p>
 
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-   <p class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white py-7">Wallet Page</p>
-
-        <table class="w-[1400px] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table
+            class="w-[1400px] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+        >
+            <thead
+                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+            >
                 <tr>
-                    <th scope="col" class="px-2 py-1">
-                        index
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                         timestamp
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        transactions
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        previousHash
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        hash
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-center">
-                        Action
-                    </th>
+                    <th scope="col" class="px-2 py-1"> index </th>
+                    <th scope="col" class="px-6 py-3"> timestamp </th>
+                    <th scope="col" class="px-6 py-3"> transactions </th>
+                    <th scope="col" class="px-6 py-3"> previousHash </th>
+                    <th scope="col" class="px-6 py-3"> hash </th>
+                    <th scope="col" class="px-6 py-3 text-center"> Action </th>
                 </tr>
             </thead>
             <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
-                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                    <td
+                        scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
                         1
                     </td>
-                    <td class="px-6 py-4">
-                        Silver
-                    </td>
-                    <td class="px-6 py-4">
-                        Laptop
-                    </td>
-                    <td class="px-6 py-4">
-                        $2999
-                    </td>
-                    <td class="px-6 py-4">
-                        dwad
-                    </td>
-                     <td class="px-6 py-4 flex justify-center">
-                       <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ">Copy Address</button>
+                    <td class="px-6 py-4"> Silver </td>
+                    <td class="px-6 py-4"> Laptop </td>
+                    <td class="px-6 py-4"> $2999 </td>
+                    <td class="px-6 py-4"> dwad </td>
+                    <td class="px-6 py-4 flex justify-center">
+                        <button
+                            type="button"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                            >Copy Address</button
+                        >
                     </td>
                 </tr>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                    <td
+                        scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
                         2
                     </td>
-                    <td class="px-6 py-4">
-                        White
-                    </td>
-                    <td class="px-6 py-4">
-                        Laptop PC
-                    </td>
-                    <td class="px-6 py-4">
-                        $1999
-                    </td>
-                    <td class="px-6 py-4">
-                       sdwad
-                    </td>
+                    <td class="px-6 py-4"> White </td>
+                    <td class="px-6 py-4"> Laptop PC </td>
+                    <td class="px-6 py-4"> $1999 </td>
+                    <td class="px-6 py-4"> sdwad </td>
                     <td class="px-6 py-4 flex justify-center">
-                       <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ">Copy Address</button>
+                        <button
+                            type="button"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                            >Copy Address</button
+                        >
                     </td>
                 </tr>
                 <tr class="bg-white dark:bg-gray-800">
-                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                       3
+                    <td
+                        scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                        3
                     </td>
-                    <td class="px-6 py-4">
-                        Black
-                    </td>
-                    <td class="px-6 py-4">
-                        Accessories
-                    </td>
-                    <td class="px-6 py-4">
-                        $99
-                    </td>
-                    <td class="px-6 py-4">
-                        dasd
-                    </td>
-                   <td class="px-6 py-4 flex justify-center">
-                       <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ">Copy Address</button>
+                    <td class="px-6 py-4"> Black </td>
+                    <td class="px-6 py-4"> Accessories </td>
+                    <td class="px-6 py-4"> $99 </td>
+                    <td class="px-6 py-4"> dasd </td>
+                    <td class="px-6 py-4 flex justify-center">
+                        <button
+                            type="button"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                            >Copy Address</button
+                        >
                     </td>
                 </tr>
             </tbody>
         </table>
     </div>
-    </body>
+</body>

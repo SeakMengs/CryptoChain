f4a024eac729c6bd54b3075cbe45ee0fecf574e1
Choung Pheng Ear <45393928+Blizzard4E@users.noreply.github.com>
2024-05-13 07:32:41 +0700

    remove some console log from server side

--- src/routes/api/transaction/create/+server.ts
+++ src/routes/api/transaction/create/+server.ts
@@ -1,13 +1,10 @@
 import { Blockchain } from "$lib/blockchain";
-import { db } from "../../../../drizzle/db.server";
 
 export async function POST(event) {
     const data = await event.request.json();
     const blockChain = new Blockchain(4);
-    console.log(data);
     await blockChain.syncChains();
     await blockChain.createPendingTransaction(data.signature, data.transaction);
-
     return new Response(
         JSON.stringify({
             success: true,
--- src/routes/api/transaction/mine/+server.ts
+++ src/routes/api/transaction/mine/+server.ts
@@ -4,7 +4,6 @@ import { db } from "../../../../drizzle/db.server";
 export async function POST(event) {
     const data = await event.request.json();
     const blockChain = new Blockchain(4);
-    console.log(data);
     await blockChain.syncChains();
     await blockChain.minePendingTransactions(data.minerWallet);
     return new Response(
